31aa4ecd15cd8498ed0431a3d47b2feb167f3f5c
freesnie <cooltraicer@gmail.com>
2024-03-27 17:24:48 +0200

    fresh
    refresh some shit

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,28 +1,42 @@
 import javafx.scene.control.TextArea;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Calculate {
 
     //поминутный уровень
-    public static void perMinute(final LocalDateTime startTime, DateTimeFormatter formatter, TextArea resultOfCalculation) {
+    public static void perMinute(TextArea resultOfCalculation, double tankVol) {
 
-        //String result = "";
+        List<String> lines = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm");
 
         for (GasStations gasStation : GasStations.leftCoast) {
 
-            LocalDateTime timeForCalc = startTime;
+            LocalDateTime timeForCalc = LocalDateTime.now();
             double lvl = gasStation.currentLevel;
+            String name = gasStation.name;
 
-            while (gasStation.currentLevel > 0) {
+            while (lvl > 0) {
 
-                lvl -= GasStations.litresPerHour(timeForCalc) / 60;
+                lvl -= GasStations.litresPerHour(timeForCalc, name) / 60;
+                tankVol -= GasStations.litresPerHour(timeForCalc, name) / 60;
                 timeForCalc = timeForCalc.plusMinutes(1);
             }
 
-            String result = timeForCalc.format(formatter) + " " + gasStation.name + " STOP";
-            resultOfCalculation.appendText(result + "\n");
+            lines.add(timeForCalc.format(formatter) + " " + gasStation.name + " STOP");
         }
+
+        Collections.sort(lines);
+        lines.add("");
+        lines.add("Залишок у цистерні: " + Math.round(tankVol) + "л");
+
+        // Выводим отсортированные строки
+        for (String line : lines) {
+            resultOfCalculation.appendText(line + "\n");
+        }
+
     }
 }
--- src/CalculateApp.java
+++ src/CalculateApp.java
@@ -1,17 +1,13 @@
 import javafx.application.Application;
 import javafx.scene.control.TextArea;
-//import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-//import javafx.scene.layout.StackPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.control.Label;
 import javafx.geometry.Insets;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class CalculateApp extends Application {
 
@@ -20,51 +16,96 @@ public class CalculateApp extends Application {
     }
 
     public void start(Stage primaryStage) {
+
         primaryStage.setTitle("CalculateApp");
 
         // Создаем кнопку "Рассчитать" и другие элементы управления
         Button calculate = new Button("Розрахувати");
 
-        //Label endTimeLabel = new Label("Уведіть кінцевий час розрахунку у форматі \"дд-мм-рр гг:хх\":");
-        Label currentLevelLabel = new Label("Уведіть залишок на АЗС " + GasStations.Krainia.name + " у %:");
+        Label curLevLabel = new Label("Уведіть залишок у %:");
+        Label tankVolLabel = new Label("Газу у цистерні, л:");
+        Label curLevLabelKrainia = new Label("Крайня:");
+        Label curLevLabelShukhevycha = new Label("Шухевича:");
+        Label curLevLabelAzerbaidzhanska = new Label("Азербайджанська:");
 
-        //TextField inputFieldEndTime = new TextField();
-        TextField inputFieldCurrentLevel = new TextField();
+        TextField curLevKrainia = new TextField();
+        curLevKrainia.setMaxWidth(30);
+        //curLevKrainia.setText("");
+
+        TextField curLevShukhevycha = new TextField();
+        curLevShukhevycha.setMaxWidth(30);
+        //curLevShukhevycha.setText("");
+
+        TextField curLevAzerbaidzhanska = new TextField();
+        curLevAzerbaidzhanska.setMaxWidth(30);
+        //curLevAzerbaidzhanska.setText("");
+        //String[] curLevFields = {String.valueOf(curLevKrainia), String.valueOf(curLevShukhevycha), String.valueOf(curLevAzerbaidzhanska)};
+
+        TextField tankVolTextField = new TextField();
+        tankVolTextField.setMaxWidth(50);
 
         TextArea resultOfCalculation = new TextArea();
         resultOfCalculation.setEditable(false);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm");
+        VBox.setMargin(curLevLabel, new Insets(5, 5, 0, 5));
 
-        //VBox.setMargin(endTimeLabel, new Insets(10, 5, 0, 5));
-        //VBox.setMargin(inputFieldEndTime, new Insets(0, 5, 10, 5));
-        VBox.setMargin(currentLevelLabel, new Insets(0, 5, 0, 5));
-        VBox.setMargin(inputFieldCurrentLevel, new Insets(0, 5, 10, 5));
+        HBox.setMargin(curLevLabelKrainia, new Insets(5, 2, 0, 5));
+        HBox.setMargin(curLevKrainia, new Insets(0, 5, 0, 0));
+
+        HBox.setMargin(curLevLabelShukhevycha, new Insets(5, 2, 0, 10));
+        HBox.setMargin(curLevShukhevycha, new Insets(0, 5, 0, 0));
+
+        HBox.setMargin(curLevLabelAzerbaidzhanska, new Insets(5, 2, 0, 10));
+        HBox.setMargin(curLevAzerbaidzhanska, new Insets(0, 5, 0, 0));
+
+        HBox.setMargin(tankVolLabel, new Insets(5, 2, 0, 5));
+        HBox.setMargin(tankVolTextField, new Insets(0, 5, 0, 0));
+
         VBox.setMargin(calculate, new Insets(0, 0, 0, 5));
-        VBox.setMargin(resultOfCalculation, new Insets(10, 5, 0, 5));
+        VBox.setMargin(resultOfCalculation, new Insets(0, 5, 0, 5));
 
-        VBox root = new VBox(5); // 10 - это расстояние между элементами
-        root.getChildren().addAll(currentLevelLabel, inputFieldCurrentLevel,
-                calculate, resultOfCalculation);
+        HBox stationArea = new HBox(0);
+        stationArea.getChildren().addAll(curLevLabelKrainia, curLevKrainia, curLevLabelShukhevycha, curLevShukhevycha,
+                curLevLabelAzerbaidzhanska, curLevAzerbaidzhanska);
+
+        HBox tankVolArea = new HBox(0);
+        tankVolArea.getChildren().addAll(tankVolLabel, tankVolTextField);
+
+        VBox vBox = new VBox(10); // 5 - это расстояние между элементами
+        vBox.getChildren().addAll(curLevLabel, stationArea, tankVolArea, calculate, resultOfCalculation);
 
         calculate.setOnAction(event -> {
+
             resultOfCalculation.clear();
-            final LocalDateTime startTime = LocalDateTime.now();
-            //final LocalDateTime startTime = LocalDateTime.parse(LocalDateTime.now().format(formatter), formatter);
-            //LocalDateTime endTime = LocalDateTime.parse(inputFieldEndTime.getText(), formatter);
-            GasStations.Krainia.currentLevel = Integer.parseInt(inputFieldCurrentLevel.getText())*100;
+/*
+            for (int i = 0; i < GasStations.leftCoast.length; i++) {
+                GasStations.leftCoast[i].currentLevel = CalculateApp.getCurLevValue(GasStations.leftCoast[i].name);
+            }
+*/
+            GasStations.Krainia.currentLevel = Integer.parseInt(curLevKrainia.getText())*100;
+            GasStations.Shukhevycha.currentLevel = Integer.parseInt(curLevShukhevycha.getText())*100;
+            GasStations.Azerbaidzhanska.currentLevel = Integer.parseInt(curLevAzerbaidzhanska.getText())*100;
+
+            double tankVol = Double.parseDouble(tankVolTextField.getText());
 
             // Вызываем метод perMinute из класса Calculate и передаем нужные значения
-            Calculate.perMinute(startTime, formatter, resultOfCalculation);
-            //String result = Calculate.perMinute(startTime, endTime, formatter, resultOfCalculation);
-            //resultOfCalculation.setText(result);
+            Calculate.perMinute(resultOfCalculation, tankVol);
         });
 
         // Создаем сцену с макетом и устанавливаем ее для primaryStage
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(vBox, 600, 400));
+        //vBox.requestFocus();
 
         // Отображаем окно
         primaryStage.show();
     }
-
+/*
+    public static Integer getCurLevValue(String name) {
+        return switch(name) {
+            case "Крайня" -> Integer.parseInt(curLevKrainia.getText())*100;
+            case "Шухевича" -> Integer.parseInt(curLevShukhevycha.getText())*100;
+            case "Азербайджанська" -> Integer.parseInt(curLevAzerbaidzhanska.getText())*100;
+        };
+    }
+*/
 }
